c86721a1b29744be90eba75ae86b3c8ebde70535
shaira-narido <65111746+shaira-narido@users.noreply.github.com>
2022-11-17 20:52:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smmothie')
-streamlit.text('Hard-Boilder Free-Range Egg')
+streamlit.header(' Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smmothie')
+streamlit.text('🐔 Hard-Boilder Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
